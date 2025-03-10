53ff3c7937780cc56f29be956711dfaee950729d
DevDevansh12 <74devanshvariya@gmail.com>
2025-03-10 15:05:56 +0530

    changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,25 @@
 import React from "react";
-
+import doctor from "../assets/doctor.png";
 import "../Css/Home.css";
 const Home = () => {
   return (
     <div className="home_container">
       <div className="main_container">
         <div className="banner">
-          <h1>Expert medical care and treatment</h1>
-          <p>
-            The ourselves suffering the sincerity. Inhabit her manners adapted
-            age certain. Debating offended at branched.
-          </p>
-          <div className="home_button">
-            <button className="home_btn">Contact Us</button>
+          <div className="home_content">
+            <h1>
+              Expert medical care and <br />
+              treatment
+            </h1>
+            <p>
+              The ourselves suffering the sincerity. Inhabit her manners adapted
+              <br />
+              age certain. Debating offended at branched.
+            </p>
+            <div className="home_button">
+              <button className="home_btn">Contact Us</button>
+            </div>
           </div>
-        </div>
-        <div className="doctor_img">
-          <img src="" alt="" />
         </div>
       </div>
     </div>
