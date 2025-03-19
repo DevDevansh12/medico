3bddbb7f4b4b19ca58034540a0d854ffd96275ef
DevDevansh12 <74devanshvariya@gmail.com>
2025-03-19 16:23:19 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Tabline from "./Components/Tabline";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 import Footer from "./Components/Footer";
+import Tabline from "./Components/Tabline";
 import About from "./Pages/About";
-import Services from "./Pages/Services";
 import Contact from "./Pages/Contact";
+import Services from "./Pages/Services";
 const App = () => {
   return (
     <>
--- src/Components/Tabline.jsx
+++ src/Components/Tabline.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import {
   FaPhoneAlt,
   FaEnvelope,
-  FaLocationDot,
-  FiCalendar,
-} from "react-icons/fa6"; // Updated imports
+  FaMapMarkerAlt,
+  FaCalendarAlt,
+} from "react-icons/fa"; // Using react-icons/fa
 import { HashLink } from "react-router-hash-link";
 
 const Tabline = () => {
   return (
-    <div className="bg-[#0CB8B6] text-white py-2 px-4 md:py-3 md:px-6 flex flex-col md:flex-row items-center justify-center">
-      {" "}
-      {/* Adjusted justify-center */}
+    <div className="bg-[#0CB8B6] text-white py-2 px-4 md:py-3 md:px-6 flex flex-col md:flex-row items-center justify-around">
       <div className="flex flex-col md:flex-row items-center mb-2 md:mb-0">
         <div className="flex items-center mb-1 md:mb-0 mr-4">
           <FaPhoneAlt className="h-4 w-4 md:h-6 md:w-6 mr-1 md:mr-2" />
@@ -26,20 +24,18 @@ const Tabline = () => {
           </span>
         </div>
         <div className="flex items-center mb-1 md:mb-0">
-          <FaLocationDot className="h-4 w-4 md:h-6 md:w-6 mr-1 md:mr-2" />
+          <FaMapMarkerAlt className="h-4 w-4 md:h-6 md:w-6 mr-1 md:mr-2" />
           <span className="text-xs md:text-sm whitespace-nowrap">
             California, United States
           </span>
         </div>
       </div>
       <HashLink
-        to="/about"
+        to="/about#appointment" //adding an appointment id to the about page.
         className="text-white font-bold text-xl font-body mt-2 md:mt-0"
       >
-        {" "}
-        {/* Adjusted margin top */}
         <button className="bg-blue-800 hover:bg-blue-700 text-white font-medium py-1 px-2 md:py-2 md:px-4 rounded flex items-center text-xs md:text-base whitespace-nowrap">
-          <FiCalendar className="mr-1 md:mr-2" />
+          <FaCalendarAlt className="mr-1 md:mr-2" />
           Make Appointment
         </button>
       </HashLink>
